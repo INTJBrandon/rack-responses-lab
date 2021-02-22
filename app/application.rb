7859395a5d1e6155b0c87ebdc4ffed36f7126245
seireitou <ausis@mail.com>
2021-02-22 15:19:23 -0600

    commit tree f739b6e2d672f71c0341abe19b6f32d3387c8e32

---
+++ app/application.rb
@@ -0,0 +1,8 @@
+class Application 
+    def call(env)
+        resp = Rack::Response.new
+        resp.write Time.now.hour < 12 ? 'Good morning' : 'Good Afternoon'
+        resp.finish
+    end
+    
+end
